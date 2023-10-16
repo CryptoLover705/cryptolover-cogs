c75755c2049226e791b0fe717c516a3c877f2811
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2023-10-16 21:00:14 +0100

    update to work with voice channels

--- cryptochannel/cryptochannel.py
+++ cryptochannel/cryptochannel.py
@@ -14,10 +14,10 @@ class CryptoChannel(commands.Cog):
                 await ctx.send("You need to specify at least one cryptocurrency symbol or ID to include.")
                 return
             await self.create_crypto_channels(ctx.guild, coins_to_include)
-            await ctx.send("Crypto info channels have been enabled for the specified coins.")
+            await ctx.send("Crypto info voice channels have been enabled for the specified coins.")
         elif action == "disable":
             await self.delete_crypto_channels(ctx.guild)
-            await ctx.send("Crypto info channels have been disabled.")
+            await ctx.send("Crypto info voice channels have been disabled.")
         else:
             await ctx.send("Invalid action. Use 'enable' or 'disable'.")
 
@@ -40,10 +40,10 @@ class CryptoChannel(commands.Cog):
                             is_price_up = price_change_24h > 0
 
                             # Create channel name and emoji
-                            channel_name = f"🪙 {coin_symbol} Price: ${price_usd:.2f}"
-                            channel_emoji = "🟢" if is_price_up else "🔴"
+                            channel_name = f"🪙 {channel_emoji} {coin_symbol} Price: ${price_usd:.2f}"
+                            channel_emoji = "🟢⬈" if is_price_up else "🔴⬊"
 
-                            # Create and configure the channel
+                            # Create and configure the voice channel
                             overwrites = {
                                 guild.default_role: discord.PermissionOverwrite(connect=False),
                                 guild.me: discord.PermissionOverwrite(manage_channels=True, connect=True),
@@ -52,8 +52,13 @@ class CryptoChannel(commands.Cog):
                             if category is None:
                                 category = await guild.create_category("Price Watch", overwrites=overwrites)
 
-                            channel = await category.create_text_channel(channel_name, reason="Crypto Info")
-                            await channel.send(f"{channel_emoji} {coin_name} ({coin_symbol}) - ${price_usd:.2f}")
+                            channel = await category.create_voice_channel(channel_name, reason="Crypto Info")
+                            await channel.set_permissions(
+                                guild.default_role,
+                                connect=False,
+                                speak=False
+                            )
+                            await channel.edit(user_limit=0)  # No user limit
                 else:
                     await guild.text_channels[0].send("Failed to fetch cryptocurrency data from Coinpaprika API.")
 
