240cc49650a54d96c7d55d9db62967a2be9fc298
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2023-10-23 03:30:46 +0100

    test

--- steam/steam.py
+++ steam/steam.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands
 from popcat_wrapper import popcat_wrapper as pop
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -11,26 +12,29 @@ class Steam(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @staticmethod
-    async def get_steam_info(name):
+    @commands.command()
+    async def steam(self, ctx, name: str):
         try:
-            s = await pop.steam(name)
-            return s
+            steam_info = await pop.steam(name)
         except Exception as e:
-            return None
+            return await ctx.send("Package not found!")
 
-@commands.command()
-async def steam(ctx, name: str):
-    steam_info = await Steam.get_steam_info(name)
+        if "name" in steam_info:  # Check if 'name' key exists in the package_info dictionary
+            last_published_str = steam_info.get('last_published', 'N/A')
+            try:
+                last_published_timestamp = datetime.strptime(last_published_str, '%a %b %d %Y')
+                last_published_formatted = f"<t:{int(last_published_timestamp.timestamp())}>"
+            except ValueError:
+                last_published_formatted = "N/A"
 
-    if steam_info is None:
-        return await ctx.send("Application not found!")
+            embed = discord.Embed(title=f"🎮・{steam_info.name}", thumbnail=steam_info.thumbnail)
+            embed.add_field(name="💬┇Name", value=steam_info.name, inline=True)
+            embed.add_field(name="📃┇Capital", value=steam_info.description, inline=False)
+            embed.add_field(name="💻┇Developers", value=", ".join(steam_info.developers), inline=True)
+            embed.add_field(name="☁┇Publishers", value=", ".join(steam_info.publishers), inline=True)
+            embed.add_field(name="🪙┇Price", value=steam_info.price, inline=True)
+            embed.add_field(name="⏰┇Published", value=last_published_formatted, inline=True)
+        else:
+            return await ctx.send("Package information format is not recognized.")
 
-    embed = discord.Embed(title=f"🎮・{steam_info.name}", thumbnail=steam_info.thumbnail)
-    embed.add_field(name="💬┇Name", value=steam_info.name, inline=True)
-    embed.add_field(name="📃┇Capital", value=steam_info.description, inline=False)
-    embed.add_field(name="💻┇Developers", value=", ".join(steam_info.developers), inline=True)
-    embed.add_field(name="☁┇Publishers", value=", ".join(steam_info.publishers), inline=True)
-    embed.add_field(name="🪙┇Price", value=steam_info.price, inline=True)
-
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
