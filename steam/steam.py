404fa3aa21e32c62a44dad75b27d93af93a693b8
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2023-10-23 02:53:57 +0100

    Update steam.py

--- steam/steam.py
+++ steam/steam.py
@@ -2,41 +2,32 @@ import discord
 from redbot.core import commands
 from popcat_wrapper import popcat_wrapper as pop
 
-
 intents = discord.Intents.default()
 intents.typing = False
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-class Steam(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def steam(self, ctx, name: str):
+class Steam:
+    @staticmethod
+    async def get_steam_info(name):
         try:
-            game_info = await pop.steam(name)
+            s = await pop.steam(name)
+            return s
         except Exception as e:
-            return await ctx.send("game not found!")
+            return None
 
-        if "name" in game_info:  # Check if 'name' key exists in the game_info dictionary
-            last_published_str = game_info.get('last_published', 'N/A')
-            try:
-                last_published_timestamp = datetime.strptime(last_published_str, '%a %b %d %Y')
-                last_published_formatted = f"<t:{int(last_published_timestamp.timestamp())}>"
-            except ValueError:
-                last_published_formatted = "N/A"
+@bot.command()
+async def steam(ctx, name: str):
+    steam_info = await Steam.get_steam_info(name)
 
-            embed = discord.Embed(title=f"📁・{game_info['name']}")
-            embed.add_field(name="💬┇Name", value=game_info['name'], inline=True)
-            embed.add_field(name="🏷️┇Version", value=game_info.get('version', 'N/A'), inline=True)
-            embed.add_field(name="📃┇Description", value=game_info.get('description', 'N/A'), inline=True)
-            embed.add_field(name="🏷️┇Price", value=game_info.get('price', 'N/A'), inline=True)
-            embed.add_field(name="💻┇Developer", value=game_info.get('developer', 'N/A'), inline=True)
-            embed.add_field(name="💻┇Publisher", value=game_info.get('publisher', 'N/A'), inline=True)
-            embed.add_field(name="📁┇Downloads", value=game_info.get('downloads_this_year', 'N/A'), inline=True)
-            embed.add_field(name="⏰┇Last publish", value=last_published_formatted, inline=True)
-        else:
-            return await ctx.send("Game is not recognized.")
+    if steam_info is None:
+        return await ctx.send("Application not found!")
 
-        await ctx.send(embed=embed)
+    embed = discord.Embed(title=f"🎮・{steam_info.name}", thumbnail=steam_info.thumbnail)
+    embed.add_field(name="💬┇Name", value=steam_info.name, inline=True)
+    embed.add_field(name="📃┇Capital", value=steam_info.description, inline=False)
+    embed.add_field(name="💻┇Developers", value=", ".join(steam_info.developers), inline=True)
+    embed.add_field(name="☁┇Publishers", value=", ".join(steam_info.publishers), inline=True)
+    embed.add_field(name="🪙┇Price", value=steam_info.price, inline=True)
+
+    await ctx.send(embed=embed)
