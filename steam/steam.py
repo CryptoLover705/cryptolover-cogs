d85aba2902f6d20663a586aa6ad64f8fc3e538ff
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2023-10-23 03:56:56 +0100

    test 2

--- steam/steam.py
+++ steam/steam.py
@@ -8,36 +8,40 @@ intents.typing = False
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@commands.command()
-async def steam(ctx, *, name: str):
-    try:
-        steam_info = await get_steam_info(name)
-    except Exception as e:
-        return await ctx.send("Game not found!")
+class Steam(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    if "name" in steam_info:
-        last_published_str = steam_info.get('last_published', 'N/A')
+    @commands.command()
+    async def steam(self, ctx, *, name: str):
         try:
-            last_published_timestamp = datetime.strptime(last_published_str, '%a %b %d %Y')
-            last_published_formatted = f"<t:{int(last_published_timestamp.timestamp())}>"
-        except ValueError:
-            last_published_formatted = "N/A"
+            steam_info = await self.get_steam_info(name)
+        except Exception as e:
+            return await ctx.send("Game not found!")
 
-        embed = discord.Embed(title=f"🎮・{steam_info['name']}", thumbnail=steam_info['thumbnail'])
-        embed.add_field(name="💬┇Name", value=steam_info['name'], inline=True)
-        embed.add_field(name="📃┇Description", value=steam_info['description'], inline=False)
-        embed.add_field(name="💻┇Developers", value=", ".join(steam_info['developers']), inline=True)
-        embed.add_field(name="☁┇Publishers", value=", ".join(steam_info['publishers']), inline=True)
-        embed.add_field(name="🪙┇Price", value=steam_info['price'], inline=True)
-        embed.add_field(name="⏰┇Published", value=last_published_formatted, inline=True)
-    else:
-        return await ctx.send("Game information format is not recognized.")
+        if "name" in steam_info:
+            last_published_str = steam_info.get('last_published', 'N/A')
+            try:
+                last_published_timestamp = datetime.strptime(last_published_str, '%a %b %d %Y')
+                last_published_formatted = f"<t:{int(last_published_timestamp.timestamp())}>"
+            except ValueError:
+                last_published_formatted = "N/A"
 
-    await ctx.send(embed=embed)
+            embed = discord.Embed(title=f"🎮・{steam_info['name']}", thumbnail=steam_info['thumbnail'])
+            embed.add_field(name="💬┇Name", value=steam_info['name'], inline=True)
+            embed.add_field(name="📃┇Description", value=steam_info['description'], inline=False)
+            embed.add_field(name="💻┇Developers", value=", ".join(steam_info['developers']), inline=True)
+            embed.add_field(name="☁┇Publishers", value=", ".join(steam_info['publishers']), inline=True)
+            embed.add_field(name="🪙┇Price", value=steam_info['price'], inline=True)
+            embed.add_field(name="⏰┇Published", value=last_published_formatted, inline=True)
+        else:
+            return await ctx.send("Game information format is not recognized.")
 
-async def get_steam_info(name):
-    try:
-        s = await pop.steam(name)
-        return s
-    except Exception as e:
-        return None
+        await ctx.send(embed=embed)
+
+    async def get_steam_info(self, name):
+        try:
+            s = await pop.steam(name)
+            return s
+        except Exception as e:
+            return None
