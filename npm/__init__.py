492e4624f8645408cb76cd5eaec06e130789c0c6
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2023-10-23 16:48:55 +0100

    delete 2 individual cogs that have been added to the search cog

--- npm/__init__.py
+++
@@ -1,5 +0,0 @@
-from .npm import Npm
-
-
-async def setup(bot):
-    await bot.add_cog(Npm(bot))
--- steam/__init__.py
+++
@@ -1,5 +0,0 @@
-from .steam import Steam
-
-
-async def setup(bot):
-    await bot.add_cog(Steam(bot))
